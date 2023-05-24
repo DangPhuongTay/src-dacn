14fde198e0678f69d995bec0415e2af42e4bf4c0
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2023-05-24 19:19:56 +0700

    update code

---
+++ travelblog-golang/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"os"
+
+	"github.com/DangPhuongTay/travelblog-golang/database"
+	"github.com/DangPhuongTay/travelblog-golang/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	database.Connect()
+	app := fiber.New()
+	app.Use(cors.New())
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	routes.Setup(app)
+	app.Listen(":" + port)
+
+}
